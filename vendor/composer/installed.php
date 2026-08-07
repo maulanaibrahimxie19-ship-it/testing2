3b919670cfbec66b59d74f4f9d28971330f4a165
maulanaibrahimxie19-ship-it <maulanaibrahimxie19@gmail.com>
2026-08-07 20:05:35 +0700

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cartpops/cartpops',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3c7c7b33ce2e87cede03bf15cd1ea4a1f10eebb2',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'cartpops/cartpops' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3c7c7b33ce2e87cede03bf15cd1ea4a1f10eebb2',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'freemius/wordpress-sdk' => array(
+            'pretty_version' => '2.12.0',
+            'version' => '2.12.0.0',
+            'reference' => 'db6f35a2b3d318a53330409dbeab49156ee76dd8',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../freemius/wordpress-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
